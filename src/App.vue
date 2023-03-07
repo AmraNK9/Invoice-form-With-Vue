814030098b39cabc5097029bb0c69afd453c15ba
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-03-07 15:40:33 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { def } from '@vue/shared';
+import product from './components/product.vue';
+</script>
+
+<template>
+  <product :product-name="productName"></product>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      productName:"Love"
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <label for="">{{ productName }}</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                productName:""
+            }
+        },
+        props:['productName']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
