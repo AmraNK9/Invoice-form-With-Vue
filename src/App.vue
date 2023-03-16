437d15bb1043c79605f35264126fc57e39bf86b9
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-03-16 12:31:18 +0630

    fist commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
 <script setup>
 import { def } from '@vue/shared';
 import product from './components/product.vue';
+
 </script>
 
 <template>
-  <product :product-name="productName"></product>
+  <div >
+    <div class=" bg-primary p-4 z-9  rounded-3 mb-2">
+      <label for="" class="form-label">Total: <h1>{{ total }} </h1> 
+
+    </label>
+    </div>
+
+  
+    <div class="container" v-for="item in productNames" :key="item.id">
+      <product @update = "upDate" :product-name="item.itemName" :value="item.value" :id = "item.id"></product>
+    </div>
+    
+  
+    
+  </div>
+
 </template>
 
 <script>
-export default{
-  data(){
-    return{
-      productName:"Love"
+export default {
+  data() {
+    return {
+      productNames: [
+        { id:1, itemName: "Love", value:1000, subTotal: 0 },
+        { id:2, itemName: "Ego", value:500, subTotal: 0 },
+        { id:3, itemName: "Hate", value:2000, subTotal: 0 },
+        { id:4, itemName: "Humenrity", value:5000, subTotal: 0}
+      ],
+      total:0
+    }
+  },
+  methods:{
+    upDate(subTotal){
+      this.total = 0
+      this.productNames[subTotal[0]-1].subTotal = subTotal[1]
+      this.productNames.forEach(
+        (obj)=>{
+          this.total += obj.subTotal
+        }
+      )
+      
     }
   }
 }
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,20 +1,60 @@
 <template>
-    <div>
-        <label for="">{{ productName }}</label>
-    </div>
-</template>
+    <div >
+        <div>
+            <label class="form-label"  for="">
+                <h1>{{ productName }}</h1>
+            </label>
+        </div>
 
+        <div>
+            <label class="form-label" for="">Quitity</label>
+            <input class="form-control"  type="number" v-model="Quitity">
+        </div> 
+
+        <div>
+            <label class="form-label" for="Price">Price</label>
+            <input class="form-control" type="text" :value="value" aria-label="Disabled input example" disabled>
+        </div>
+         <div>
+            <label for="form-label" class="form-label" >Sub Total</label>
+            <label for="" class="form-label">
+                <h2>{{ subTotal }}</h2>
+            </label>
+         </div>
+    </div>
+    
+
+    
+    
+  
+</template>
+    
 <script>
+    import "bootstrap/dist/css/bootstrap.min.css"
     export default {
         data(){
             return{
-                productName:""
+              Quitity:0,
+              subTotal:0,
+                
             }
         },
-        props:['productName']
+        props:['productName',"value","id"],
+        watch:{
+            Quitity(qty){
+                this.subTotal = qty * this.value
+                this.$emit("update",[this.id,this.subTotal])
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 
+div{
+    margin-bottom: 5px;
+}
+.container{
+    margin: 20px;
+}
 </style>
